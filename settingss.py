19f0eed8b6dae010176deba235e7cdd38ae6f451
Jeevan Syriac <jeevansyriac1999@gmail.com>
2022-04-30 22:46:15 +0530

    first commit

---
+++ settingss.py
@@ -0,0 +1,334 @@
+from ast import pattern
+from calendar import c
+from cgitb import enable, text
+from distutils import command
+from itertools import count
+from pydoc import describe
+from secrets import choice
+from sqlite3 import enable_callback_tracebacks
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+from tkinter.font import BOLD
+from urllib.parse import parse_qs
+from PIL import ImageTk, Image, ImageFile
+from matplotlib.font_manager import json_dump
+from numpy import choose, empty, place
+import pandas as pd
+from tkinter.messagebox import showinfo
+import tkinter.scrolledtext as scrolledtext
+from tkinter.filedialog import askopenfilename
+import os
+import webbrowser
+from pip import main
+from tkcalendar import Calendar
+from tkcalendar import DateEntry
+from datetime import date
+from tkinter import filedialog
+import subprocess
+import mysql.connector
+import io
+from openpyxl.workbook import Workbook
+from openpyxl import load_workbook
+import shutil
+import csv
+import json
+
+#sql connection
+fbilldb = mysql.connector.connect(
+    host="localhost", user="root", password="", database="fbill", port="3306"
+)
+fbcursor = fbilldb.cursor()
+#------------#
+
+
+root=Tk()
+root.geometry("1360x730")
+root.resizable(False, False)
+root.title("F-Billing Revolution 2022(FREE version) | Company database:fbillingdb | User:Administrator")
+p1 = PhotoImage(file = 'images/fbicon.png')
+root.iconphoto(False, p1)
+
+
+s = ttk.Style()
+s.theme_use('default')
+s.configure('TNotebook.Tab', background="#999999", width=20, padding=10)
+invoices= PhotoImage(file="images/invoice.png")
+orders = PhotoImage(file="images/order.png")
+estimates = PhotoImage(file="images/estimate.png")
+recurring = PhotoImage(file="images/recurring.png")
+purchase = PhotoImage(file="images/purchase.png")
+expenses = PhotoImage(file="images/expense.png")
+customer = PhotoImage(file="images/customer.png")
+product = PhotoImage(file="images/package.png")
+reports = PhotoImage(file="images/report.png")
+setting = PhotoImage(file="images/setting.png")
+tick = PhotoImage(file="images/check.png")
+warnin = PhotoImage(file="images/sign_warning.png")
+cancel = PhotoImage(file="images/close.png")
+saves = PhotoImage(file="images/save.png")
+folder = PhotoImage(file="images/folder-black.png")
+photo11 = PhotoImage(file = "images/invoice-pvt.png")
+customer = PhotoImage(file="images/customer.png")
+smslog = PhotoImage(file = "images/smslog.png")
+video = PhotoImage(file = "images/video.png")
+mark1 = PhotoImage(file="images/mark.png")
+mark2 = PhotoImage(file="images/mark2.png")
+photo10 = PhotoImage(file = "images/text-message.png")
+addnew = PhotoImage(file="images/plus.png")
+delete = PhotoImage(file="images/delete_E.png")
+tabControl = ttk.Notebook(root)
+tab1 = ttk.Frame(tabControl)
+tab2 = ttk.Frame(tabControl)
+tab3=  ttk.Frame(tabControl)
+tab4 = ttk.Frame(tabControl)
+tab5 = ttk.Frame(tabControl)
+tab6=  ttk.Frame(tabControl)
+tab7 = ttk.Frame(tabControl)
+tab8 = ttk.Frame(tabControl)
+tab9 =  ttk.Frame(tabControl)
+tab10=  ttk.Frame(tabControl)
+tabControl.add(tab1,image=invoices,compound = LEFT, text ='Invoices',)
+tabControl.add(tab2,image=orders,compound = LEFT, text ='Orders')
+tabControl.add(tab3,image=estimates,compound = LEFT, text ='Estimates')
+tabControl.add(tab4,image=recurring,compound = LEFT, text ='Recurring')
+tabControl.add(tab5,image=purchase,compound = LEFT, text ='Purchase Orders') 
+tabControl.add(tab6,image=expenses,compound = LEFT, text ='Expenses')
+tabControl.add(tab7,image=customer,compound = LEFT, text ='Customers')
+tabControl.add(tab8,image=product,compound = LEFT, text ='Product/Services')
+tabControl.add(tab9,image=reports,compound = LEFT, text ='Report')
+tabControl.add(tab10,image=setting,compound = LEFT, text ='Settings')
+tabControl.pack(expand = 1, fill ="both")
+
+
+selectall = PhotoImage(file="images/table_select_all.png")
+cut = PhotoImage(file="images/cut.png")
+copy = PhotoImage(file="images/copy.png")
+paste = PhotoImage(file="images/paste.png")
+
+undo = PhotoImage(file="images/undo.png")
+redo = PhotoImage(file="images/redo.png")
+bold = PhotoImage(file="images/bold.png")
+
+italics = PhotoImage(file="images/italics.png")
+underline = PhotoImage(file="images/underline.png")
+left = PhotoImage(file="images/left.png")
+
+right = PhotoImage(file="images/right.png")
+center = PhotoImage(file="images/center.png")
+hyperlink = PhotoImage(file="images/hyperlink.png")
+remove = PhotoImage(file="images/eraser.png")
+
+
+photo = PhotoImage(file = "images/plus.png")
+photo1 = PhotoImage(file = "images/edit.png")
+photo2 = PhotoImage(file = "images/delete_E.png")
+photo3 = PhotoImage(file = "images/export-file.png")
+photo4 = PhotoImage(file = "images/seo.png")
+photo5 = PhotoImage(file = "images/printer.png")
+photo6 = PhotoImage(file = "images/gmail.png")
+photo7 = PhotoImage(file = "images/priewok.png")
+photo8 = PhotoImage(file = "images/refresh_E.png")
+photo9 = PhotoImage(file = "images/sum.png")
+photo10 = PhotoImage(file = "images/text-message.png")
+
+settingsframe=Frame(tab10, relief=GROOVE, bg="#f8f8f2")
+settingsframe.pack(side="top", fill=BOTH)
+  
+settframe=Frame(settingsframe, bg="#f5f3f2", height=60)
+settframe.pack(side="top", fill=X)
+
+pn = Canvas(settframe, width=1, height=65, bg="#b3b3b3", bd=0)
+pn.pack(side="left", padx=(5, 2))
+pn = Canvas(settframe, width=1, height=65, bg="#b3b3b3", bd=0)
+pn.pack(side="left", padx=(0, 5))
+
+    
+addcustomerIcon = ImageTk.PhotoImage(Image.open("images/user_add.png"))
+addcustomerLabel = Button(settframe,compound="top", text="Save\nSettings",relief=RAISED,    command="",image=saves, font=("arial", 8),bg="#f5f3f2", fg="black", height=55, bd=1, width=55)
+addcustomerLabel.pack(side="left", pady=3, ipadx=4)
+pn = Canvas(settframe, width=1, height=65, bg="#b3b3b3", bd=0)
+pn.pack(side="left", padx=(0, 5))
+
+editcustomerIcon = ImageTk.PhotoImage(Image.open("images/user_edit.png"))
+editcustomerLabel = Button(settframe,compound="top", text="Quick\nStart Wizard",relief=RAISED,command="",  image=editcustomerIcon,  font=("arial", 8),bg="#f8f8f2", fg="black", height=55, bd=1, width=55)
+editcustomerLabel.pack(side="left")
+
+deletecustomerIcon = ImageTk.PhotoImage(Image.open("images/user_delete.png"))
+deletecustomerLabel = Button(settframe,compound="top", text="Company\nManager",relief=RAISED, command="", image=deletecustomerIcon, font=("arial", 8),bg="#f8f8f2", fg="black", height=55, bd=1, width=55)
+deletecustomerLabel.pack(side="left")
+
+pn = Canvas(settframe, width=1, height=65, bg="#b3b3b3", bd=0)
+pn.pack(side="left", padx=5)
+
+previewinvoiceIcon = ImageTk.PhotoImage(Image.open("images/priewok.png"))
+previewinvoiceLabel = Button(settframe,compound="top",command="", text="Optimize\nData tables", relief=RAISED,               image=previewinvoiceIcon, font=("arial", 8),bg="#f8f8f2", fg="black",  height=55, bd=1, width=55)
+previewinvoiceLabel.pack(side="left")
+
+printinvoiceIcon = ImageTk.PhotoImage(Image.open("images/printer.png"))
+printinvoiceLabel = Button(settframe,compound="top", text="Repair\nDatabase",relief=RAISED,  command="",  image=printinvoiceIcon, font=("arial", 8),bg="#f8f8f2", fg="black", height=55, bd=1, width=55)
+printinvoiceLabel.pack(side="left")
+
+pn = Canvas(settframe, width=1, height=65, bg="#b3b3b3", bd=0)
+pn.pack(side="left", padx=5)
+
+emailinviceIcon = ImageTk.PhotoImage(Image.open("images/gmail.png"))
+emailinviceLabel = Button(settframe,compound="top",command="", text="Backup\nDatabase", relief=RAISED,               image=emailinviceIcon, font=("arial", 8),bg="#f8f8f2", fg="black",height=55,   bd=1, width=55)
+emailinviceLabel.pack(side="left")
+
+refreshcustomerIcon = ImageTk.PhotoImage(Image.open("images/refresh.png"))
+refreshcustomerLabel = Button(settframe,compound="top", command="",text="Restore\nDatabase", relief=RAISED,               image=refreshcustomerIcon, font=("arial", 8),bg="#f8f8f2", fg="black",  height=55, bd=1, width=55)
+refreshcustomerLabel.pack(side="left")
+
+pn = Canvas(settframe, width=1, height=65, bg="#b3b3b3", bd=0)
+pn.pack(side="left", padx=5)
+
+smsIcon = ImageTk.PhotoImage(Image.open("images/text-message.png"))
+smsLabel = Button(settframe,compound="top", text="Serach\nfor Updates",command="", relief=RAISED,  image=smsIcon, font=("arial", 8),bg="#f8f8f2", fg="black", height=55, bd=1, width=55)
+smsLabel.pack(side="left")
+
+pn = Canvas(settframe, width=1, height=65, bg="#b3b3b3", bd=0)
+pn.pack(side="left", padx=5)
+
+importcustomerIcon = ImageTk.PhotoImage(Image.open("images/import.png"))
+importcustomerLabel = Button(settframe,compound="top", text="Enter licence\nKey Code",command="", relief=RAISED, image=importcustomerIcon, font=("arial", 8),bg="#f8f8f2", fg="black", height=55, bd=1,  width=55)
+importcustomerLabel.pack(side="left")
+
+pn = Canvas(settframe, width=1, height=65, bg="#b3b3b3", bd=0)
+pn.pack(side="left", padx=5)
+
+exportcustomerIcon = ImageTk.PhotoImage(Image.open("images/export.png"))
+exportcustomerLabel = Button(settframe,compound="top", text="Online\nUser Manual",command="",relief=RAISED,   image=exportcustomerIcon, font=("arial", 8),bg="#f8f8f2", fg="black", height=55, bd=1,width=55)
+exportcustomerLabel.pack(side="left")
+
+pn = Canvas(settframe, width=1, height=65, bg="#b3b3b3", bd=0)
+pn.pack(side="left", padx=5)
+
+customersearchIcon = ImageTk.PhotoImage(Image.open("images/search-icon.png"))
+customersearchLabel = Button(settframe,compound="top",command="", text="Upgrade to\nPro Now!", relief=RAISED,               image=customersearchIcon, font=("arial", 8),bg="#f8f8f2", fg="black",  height=55, bd=1, width=55)
+customersearchLabel.pack(side="left")
+
+
+invoi1label = Label(settingsframe, text="Settings", font=("arial", 18), bg="#f8f8f2")
+invoi1label.pack(side="left", padx=(20,0))
+
+m = ttk.Style()
+m.theme_use('default')
+m.configure('one.TNotebook.Tab', background="white", width=20, padding=10)
+tabControl = ttk.Notebook(tab10,style='one.TNotebook.Tab')
+tab01 = ttk.Frame(tabControl)
+tab02 = ttk.Frame(tabControl)
+tab03=  ttk.Frame(tabControl)
+tab04 = ttk.Frame(tabControl)
+tab05 = ttk.Frame(tabControl)
+tab06=  ttk.Frame(tabControl)
+tab07 = ttk.Frame(tabControl)
+tab08 = ttk.Frame(tabControl)
+tab09 =  ttk.Frame(tabControl)
+tab010=  ttk.Frame(tabControl)
+tabControl.add(tab01,image=invoices,compound = LEFT, text ='Miscellaneous',)
+tabControl.add(tab02,image=orders,compound = LEFT, text ='Company settings')
+tabControl.add(tab03,image=estimates,compound = LEFT, text ='Invoiced settings')
+tabControl.add(tab04,image=recurring,compound = LEFT, text ='Order settings')
+tabControl.add(tab05,image=purchase,compound = LEFT, text ='Estimate settings') 
+tabControl.add(tab06,image=expenses,compound = LEFT, text ='Administrator panel')
+tabControl.add(tab07,image=customer,compound = LEFT, text ='Advanced settings')
+tabControl.add(tab08,image=product,compound = LEFT, text ='Email templates')
+tabControl.add(tab09,image=reports,compound = LEFT, text ='Payments')
+tabControl.add(tab010,image=setting,compound = LEFT, text ='Purchase Order')
+tabControl.pack(expand = 1, fill ="both")
+
+
+
+
+#--------tab3-------------#
+firsttab1=Frame(tab03, relief=GROOVE, bg="#f8f8f2")
+firsttab1.pack(side="top", fill=BOTH)
+
+firsttab=Frame(firsttab1, bg="#f5f3f2", height=700)
+firsttab.pack(side="top", fill=BOTH)
+
+ver = Label(firsttab,text="Invoice# prefix")
+ver.place(x=5,y=40)
+
+lbx = Listbox(firsttab1, height=1)
+lbx.insert(END, "EST")
+lbx.place(x=100,y=40)
+
+ver = Label(firsttab,text="Starting Invoice number")
+ver.place(x=25,y=80)
+
+spin1 = Spinbox(firsttab,from_=1,to=1000000,width=15)
+spin1.place(x=50,y=100)
+
+ver = Label(firsttab,text="Header box background color")
+ver.place(x=5,y=140)
+
+
+win_menu = StringVar()
+winstyle = ttk.Combobox(firsttab,textvariable=win_menu)
+winstyle.place(x=6 ,y=160)
+winstyle['values'] = ('Default','Black','Maroon','Green','Olive','Navy','Purple','Teal','Gray','Silver','Red','Lime','Yellow','Blue','Fuchsia','Aqua','White','ScrollBar','Background','ActiveCaption','InactiveCaption','Menu','Window','WindowFrame','MenuText','WindowText','CaptionText','ActiveBorder','InactiveBorder','AppWorkSpace','Highlight','HighlightText','BtnFace','InactiveCaptionText','BtnHighlight','3DDkShadow','3DLight','InfoText','InfoBk','Custom')
+winstyle.current(0)
+
+ver = Label(firsttab,text="Customize Invoice text labels")
+ver.place(x=5,y=190)
+
+lbx = Listbox(firsttab1, height=8, width=25)
+lbx.insert(END, "Invoice")
+lbx.insert(END, "Invoice#")
+lbx.insert(END, "Invoice date")
+lbx.insert(END, "Due date")
+lbx.insert(END, "Order ref.#")
+lbx.insert(END, "Terms")
+lbx.insert(END, "Invoice to")
+lbx.insert(END, "Ship to")
+lbx.insert(END, "ID/SKU")
+lbx.insert(END, "Product/Service")
+lbx.insert(END, "Quantity")
+lbx.insert(END, "Description")
+lbx.insert(END, "Unit Price")
+lbx.insert(END, "Price")
+lbx.insert(END, "Subtotal")
+lbx.insert(END, "Discount")
+lbx.insert(END, "Discount rate")
+lbx.insert(END, "TAX1")
+lbx.insert(END, "TAX2")
+lbx.insert(END, "Total Paid")
+lbx.insert(END, "Balance")
+lbx.insert(END, "Terms and Conditions")
+lbx.insert(END, "Tax Exempted")
+lbx.insert(END, "Page")
+lbx.insert(END, "of")
+
+
+
+lbx.place(x=5,y=220)
+
+ver = Label(firsttab,text="Default Invoice template(example,click on preview for mouse scrolling)")
+ver.place(x=248,y=55 )
+
+ver = Label(firsttab,text="Default Invoice template")
+ver.place(x=619,y=40)
+
+win_menu = StringVar()
+winstyle = ttk.Combobox(firsttab,textvariable=win_menu)
+winstyle.place(x=770 ,y=40, width=220)
+winstyle['values'] = ('Professional 1 (logo on left side)','Professional 2 (logo on right side)','Simplified 1 (logo on left side)','Simplified 2 (logo on right side)','Business Classic')
+winstyle.current(0)
+
+messagelbframe=LabelFrame(firsttab,text="Predefined terms and conditions text for estimates", height=100, width=980)
+messagelbframe.place(x=248, y=400)
+
+txt = scrolledtext.ScrolledText(firsttab, undo=True,width=115,height=4)
+txt.place(x=260,y=425)
+
+bttermadd = Button(firsttab,text="Restore defaults")
+bttermadd.place(x=32,y=450)
+
+lbx = Listbox(firsttab1, height=18, width=162, bg="grey")
+lbx.place(x=248,y=90)
+
+
+
+root.mainloop()
